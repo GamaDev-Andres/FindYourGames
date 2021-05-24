35a438d75d87c73431db2dc505a91a03a0b81756
Andres Gama <gama_pipe@hotmail.com>
2021-05-24 16:26:10 -0500

    le di funcionalidad al boton scroll

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,9 +1,11 @@
 const iconoMenu = document.querySelector(".menu");
+//DOMContentLoaded
 document.addEventListener("DOMContentLoaded", () => {
   iconoMenu.addEventListener("click", desplegarMenu);
   consultarAPI();
+  detectaScroll();
 });
-
+//FUNCION PARA DESPLEGAR EL MENU ,CUANDO DOY CLICK EN EL ICONO
 function desplegarMenu() {
   //mostrar menu desplegable
   const enlaces = document.querySelector(".enlaces");
@@ -19,6 +21,7 @@ function desplegarMenu() {
     iconoMenu.classList.add("inactivo");
   }
 }
+//CONSULTO A LA API
 async function consultarAPI() {
   const urlList = "https://www.freetogame.com/api/games";
   const url = `https://api.allorigins.win/get?url=${encodeURIComponent(
@@ -31,27 +34,33 @@ async function consultarAPI() {
 }
 
 let inicial = 0;
-
+//CREA EL HTML
 function crearHtml(resultado) {
+  //VARIABLE PARA LLEVAR LA CUENTA TOTAL DEL ARRAY ORIGINAL
   let fin = 30 + inicial;
+  //CONTENEDOR PRINCIPAL
   const contenidoPrincipal = document.querySelector(".contenido-gameList");
   const array = JSON.parse(resultado.contents);
-  //   console.log(arrayGames);
+  //SEPARO EL ARRAY EN 30
   let arrayGames = array.slice(inicial, fin);
-  console.log(arrayGames);
+
+  //ITERO EL ARRAY QUE SEPARE
   arrayGames.forEach((game, index) => {
     inicial++;
+
     const contenedorJuegos = crearHtmlGame(game);
-    // console.log(contenedorJuegos);
+    //CONDICIONAL PARA CUANDO SE ACABE EL ARRAY
     if (inicial === array.length) {
+      const aviso = document.querySelector(".aviso-contenido");
       console.log("no hay mas");
-      // console.log("SI ENTRE A ESTA PARTE");
       let noMore = document.createElement("h3");
+      noMore.classList.add("aviso");
       noMore.textContent = "No Hay Mas Contenido";
-      contenidoPrincipal.appendChild(noMore);
       contenidoPrincipal.appendChild(contenedorJuegos);
+      aviso.appendChild(noMore);
       return;
     }
+    //CONDICIONAL PARA CUANDO TERMINE DE ITERAR
     if (index === 29) {
       console.log(contenedorJuegos);
       setObserver();
@@ -59,12 +68,14 @@ function crearHtml(resultado) {
     contenidoPrincipal.appendChild(contenedorJuegos);
   });
 }
+//EL CALLBACK DEL OBSERVER , ME PERMITE CARGAR MAS IMGS
 function cargarMas(entry) {
   console.log(entry);
   if (entry[0].isIntersecting) {
     consultarAPI();
   }
 }
+//FUNCION DEL OBSERVER
 function setObserver() {
   const contenidoPrincipal = document.querySelector(".contenido-gameList");
 
@@ -74,6 +85,7 @@ function setObserver() {
   const observador = new IntersectionObserver(cargarMas, options);
   observador.observe(contenidoPrincipal.lastChild);
 }
+//FUNCION QUE CREA EL HTML
 function crearHtmlGame(game) {
   const { title, id, thumbnail, short_description, game_url } = game;
 
@@ -105,9 +117,28 @@ function crearHtmlGame(game) {
   boton.target = "_blank";
   boton.textContent = "JUEGA AHORA!";
 
-  informacion.prepend(strong);
   divInfo.append(titulo, informacion, boton);
   divGame.append(divImg, divInfo);
   fragment.appendChild(divGame);
   return fragment;
+}
+
+//////////////////////////////////////////////////////////
+function detectaScroll() {
+  document.addEventListener("scroll", () => {
+    console.log(pageYOffset);
+    let scrollY = pageYOffset;
+    const btnScroll = document.querySelector(".btn-scroll");
+    if (scrollY < 1000) {
+      btnScroll.style.display = "none";
+    } else {
+      btnScroll.style.display = "block";
+      btnScroll.addEventListener("click", () => {
+        scroll({
+          behavior: "smooth",
+          top: 0,
+        });
+      });
+    }
+  });
 }
